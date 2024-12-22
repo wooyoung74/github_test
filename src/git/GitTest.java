4bc8e72d36a4dbf0f73bdea319fb594b44d50958
wooyoung74 <djadndud@hanmail.net>
2024-12-22 18:40:46 +0900

    Update GitTest.java

--- src/git/GitTest.java
+++ src/git/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("test....");
+		System.out.print("testeeee....");
+		
 	}
 
 }
